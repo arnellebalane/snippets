283315b7d68040a0cc5bb0c464d6fff467ff48ca
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 07:33:11 +0800

    build(webpack): add missing file extension to entrypoint script

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'source/index'),
+    entry: path.resolve(__dirname, 'source/index.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
