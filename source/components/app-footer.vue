613de0853de6a413ad0512b45be269682339ccd9
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 09:12:01 +0800

    feat(footer): define app-footer vue component

---
+++ source/components/app-footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer class="app-footer">
+        <ul>
+            <li><span>^S</span> Save</li>
+        </ul>
+    </footer>
+</template>
+
+<style scoped>
+    .app-footer {
+        padding: 1px 0;
+        color: #c5c8c6;
+    }
+
+    ul {
+        padding: 0;
+        margin: 0;
+        list-style: none;
+    }
+
+    span {
+        color: #1d1f21;
+        background-color: #c5c8c6;
+    }
+
+    li:first-child span {
+        padding-left: 5px;
+    }
+</style>
--- source/components/snippets-app.vue
+++ source/components/snippets-app.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="snippets-app">
         <app-header></app-header>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
     export default {
         components: {
-            'app-header': require('./app-header.vue').default
+            'app-header': require('./app-header.vue').default,
+            'app-footer': require('./app-footer.vue').default
         }
     };
 </script>
+
+<style scoped>
+    .app-footer {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
