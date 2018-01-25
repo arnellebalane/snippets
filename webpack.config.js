fcace2a4c69027f7c93e6a3eee38c97ddab28bd0
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 14:00:49 +0800

    build(webpack): add babel-polyfill to webpack config entries

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'source/index.js'),
+    entry: [
+        'babel-polyfill',
+        path.resolve(__dirname, 'source/index.js')
+    ],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
