0af276d34f1a1315e657cd6376969b7c9e6631c1
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 09:22:16 +0800

    feat(snippet): define app-snippet vue component

---
+++ source/components/app-snippet.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="app-snippet">
+        <textarea name="snippet" placeholder="console.log('hello world')" spellcheck="false" autofocus></textarea>
+    </section>
+</template>
+
+<style scoped>
+    textarea {
+        display: block;
+        width: 100%;
+        height: 100%;
+        padding: 3px 5px;
+        border: none;
+        font: inherit;
+        line-height: 14px;
+        color: #c5c8c6;
+        background-color: transparent;
+        outline: none;
+        resize: none;
+    }
+</style>
--- source/components/snippets-app.vue
+++ source/components/snippets-app.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="snippets-app">
         <app-header></app-header>
+        <app-snippet></app-snippet>
         <app-footer></app-footer>
     </div>
 </template>
@@ -9,12 +10,21 @@
     export default {
         components: {
             'app-header': require('./app-header.vue').default,
+            'app-snippet': require('./app-snippet.vue').default,
             'app-footer': require('./app-footer.vue').default
         }
     };
 </script>
 
 <style scoped>
+    .app-snippet {
+        position: fixed;
+        top: 16px;
+        left: 0;
+        right: 0;
+        bottom: 16px;
+    }
+
     .app-footer {
         position: fixed;
         left: 0;
