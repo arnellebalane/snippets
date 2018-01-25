ca0e9d6a05052ae1f3ec7cc06ae439fd51cb68f5
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 13:14:38 +0800

    feat(router): add router object to vue instance

--- source/index.js
+++ source/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import SnippetsApp from './components/snippets-app.vue';
 
 import './stylesheets/index.css';
 
+Vue.use(VueRouter);
+
 new Vue({
     el: '#app',
+    router: require('./router').default,
     render: h => h(SnippetsApp)
 });
---
+++ source/router.js
@@ -0,0 +1,10 @@
+import VueRouter from 'vue-router';
+
+const routes = [];
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+});
+
+export default router;
