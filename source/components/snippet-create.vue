93eb89927cdc58ad87b5e8d856a1d91d7686a102
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 13:30:29 +0800

    refactor: define template for snippet-create component

--- source/components/snippet-create.vue
+++ source/components/snippet-create.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="snippet-create">
-        <h1>Create Snippet</h1>
+        <app-header></app-header>
+        <code-snippet></code-snippet>
+        <app-footer></app-footer>
     </div>
 </template>
+
+<script>
+    export default {
+        components: {
+            'app-header': require('./app-header.vue').default,
+            'app-footer': require('./app-footer.vue').default,
+            'code-snippet': require('./code-snippet.vue').default
+        }
+    };
+</script>
+
+<style scoped>
+    .code-snippet {
+        position: fixed;
+        top: 16px;
+        left: 0;
+        right: 0;
+        bottom: 16px;
+    }
+
+    .app-footer {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
