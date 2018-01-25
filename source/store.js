119f722ea6b83fa38c0fc6f26368069322aa5781
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 18:00:58 +0800

    feat(store): add mutation for setting the snippet state

--- source/store.js
+++ source/store.js
@@ -3,6 +3,12 @@ import Vuex from 'vuex';
 const store = new Vuex.Store({
     state: {
         snippet: null
+    },
+
+    mutations: {
+        setSnippet(state, snippet) {
+            state.snippet = snippet;
+        }
     }
 });
 
