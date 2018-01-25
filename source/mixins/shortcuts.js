cc07f3fe5be2223c1fcab06ffcea25c64b84610e
Arnelle Balane <arnellebalane@gmail.com>
2018-01-25 16:36:57 +0800

    fix(shortcuts): remove keydown event handlers when component is destroyed

--- source/mixins/shortcuts.js
+++ source/mixins/shortcuts.js
@@ -2,17 +2,23 @@ export default {
     methods: {
         getShortcuts() {
             return {};
-        }
-    },
+        },
 
-    created() {
-        const shortcuts = this.getShortcuts();
+        _keydownHandler(e) {
+            const shortcuts = this.getShortcuts();
 
-        window.addEventListener('keydown', e => {
             if (e.ctrlKey && e.code in shortcuts) {
                 e.preventDefault();
                 shortcuts[e.code]();
             }
-        });
+        }
+    },
+
+    created() {
+        window.addEventListener('keydown', this._keydownHandler);
+    },
+
+    destroyed() {
+        window.removeEventListener('keydown', this._keydownHandler);
     }
 };
