d00b95939b1c215c1ee35e716a49b08f8a18536c
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 11:48:32 +0800

    fix(webpack): wrap NODE_ENV value in extra quotes
    https://github.com/thgh/vuejs-templates-rollup/issues/1

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: process.env.NODE_ENV || 'development'
+                NODE_ENV: `'${process.env.NODE_ENV}'` || '"development"'
             }
         }),
         new UglifyJsPlugin()
