3b30090eb75cef52c4d577f75c0dca3c9440551f
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 10:17:11 +0800

    refactor: disable database query logging

--- database/index.js
+++ database/index.js
@@ -3,6 +3,8 @@ const config = require('./config');
 
 const env = process.env.NODE_ENV || 'development';
 const connectionConfig = config[env].url;
-const db = new Sequelize(connectionConfig);
+const db = new Sequelize(connectionConfig, {
+    logging: false
+});
 
 module.exports = db;
