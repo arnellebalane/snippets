129ce564e892157a14132ef983202b91a99e660e
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 09:24:11 +0800

    feat(db): create sequelize database instance

---
+++ database/index.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+const env = process.env.NODE_ENV || 'development';
+const connectionConfig = config[env].url;
+const db = new Sequelize(connectionConfig);
+
+module.exports = db;
