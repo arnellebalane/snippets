f450ccc54ac3d9cec9b821295880541397f1eb6c
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 08:44:31 +0800

    refactor(api): combine get snippet api and html routes

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const { get, post, error } = server.router;
 const { status, render, send, json } = server.reply;
 
 server(
-    get('/:hash?', ctx => render('index.html')),
+    get('/', ctx => render('index.html')),
+
+    get('/:hash', ctx => {
+        if (ctx.headers.accept === 'application/json') {
+            return json({
+                id: 1,
+                hash: ctx.params.hash
+            });
+        }
+        return render('index.html');
+    }),
 
     post('/snippets', ctx => {
         return json({
             id: 1,
             hash: 'abcde'
-        });
-    }),
-
-    get('/snippets/:hash', ctx => {
-        return json({
-            id: 1,
-            hash: ctx.params.hash
         });
     }),
 
