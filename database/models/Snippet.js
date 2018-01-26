a6dcb03eccfac5b719fb0375e6f06de2d77844ad
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 09:54:25 +0800

    feat(db): generate sniippet hash inside beforeValidate hook

--- database/models/Snippet.js
+++ database/models/Snippet.js
@@ -1,9 +1,35 @@
+const crypto = require('crypto');
+
 module.exports = (database, DataTypes) => {
-    return database.define('snippet', {
+    const Snippet = database.define('snippet', {
         hash: {
             type: DataTypes.STRING,
             primaryKey: true
         },
         body: DataTypes.TEXT
+    }, {
+        hooks: {
+            async beforeValidate(instance, options) {
+                let body = instance.body + Date.now();
+                let hash = crypto.createHash('sha256')
+                    .update(body)
+                    .digest('base64')
+                    .substring(0, 5);
+                let existing = await Snippet.findOne({ where: { hash } });
+
+                while (existing) {
+                    body = instance.body + Date.now();
+                    hash = crypto.createHash('sha256')
+                        .update(body)
+                        .digest('base64')
+                        .substring(0, 5);
+                    existing = await Snippet.findOne({ where: { hash } });
+                }
+
+                instance.hash = hash;
+            }
+        }
     });
+
+    return Snippet;
 };
