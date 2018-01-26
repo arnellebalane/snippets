1878912ec80d622dfc9209874d71976d5dffe168
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 11:50:59 +0800

    build(webpack): enable sourceMap option in uglifyjs plugin

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,8 @@ module.exports = {
                 NODE_ENV: `'${process.env.NODE_ENV}'` || '"development"'
             }
         }),
-        new UglifyJsPlugin()
+        new UglifyJsPlugin({
+            sourceMap: true
+        })
     ]
 };
