a80056a1636c833c2c88d978aebdc47bb1b17d1e
Arnelle Balane <arnellebalane@gmail.com>
2018-01-26 09:32:30 +0800

    feat(db): autodiscover database model definitions

---
+++ database/models/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const db = require('../index');
+
+const modelsPath = __dirname;
+const baseName = path.basename(__filename);
+
+const models = fs.readdirSync(modelsPath)
+    .filter(fileName => fileName !== baseName)
+    .reduce((models, modelFile) => {
+        const modelName = path.basename(modelFile, '.js');
+        models[modelName] = db.import(path.join(modelsPath, modelFile));
+        return models;
+    }, {});
+
+module.exports = models;
