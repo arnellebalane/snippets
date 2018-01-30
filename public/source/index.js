55eb0a9aa190a4a70ba51a9768dcacf53defb433
Arnelle Balane <arnellebalane@gmail.com>
2018-01-30 20:49:03 +0800

    refactor: move Vue.use calls inside router and store definitions

--- public/source/index.js
+++ public/source/index.js
@@ -1,16 +1,13 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
-import VueRouter from 'vue-router';
+import router from './router';
+import store from './store';
 import SnippetsApp from './components/snippets-app.vue';
 
 import './stylesheets/index.css';
 
-Vue.use(Vuex);
-Vue.use(VueRouter);
-
 new Vue({
     el: '#app',
-    store: require('./store').default,
-    router: require('./router').default,
+    store,
+    router,
     render: h => h(SnippetsApp)
 });
