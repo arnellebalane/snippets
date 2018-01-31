4a0b1f9b4f4a7190922736fcfad3faa45109358e
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 11:15:44 +0800

    feat: attach store object in vue app via factory function

--- source/app.js
+++ source/app.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import createRouter from './router';
+import createStore from './store';
 import SnippetsApp from './components/snippets-app.vue';
 
 export default function createApp() {
     const router = createRouter();
+    const store = createStore();
 
     const app = new Vue({
         router,
+        store,
         render: h => h(SnippetsApp)
     });
 
-    return { app, router };
+    return { app, router, store };
 };
