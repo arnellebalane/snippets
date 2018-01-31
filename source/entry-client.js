ed563cba2b904b824f17182eda92481cc303e857
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 17:03:52 +0800

    feat: pick up ssr initial state before mounting app in client-side

--- source/entry-client.js
+++ source/entry-client.js
@@ -3,6 +3,10 @@ import './stylesheets/index.css';
 
 const { app, router, store } = createApp();
 
+if (window.__INITIAL_STATE__) {
+    store.replaceState(window.__INITIAL_STATE__);
+}
+
 router.onReady(() => {
     router.beforeResolve((to, from, next) => {
         const matched = router.getMatchedComponents(to);
