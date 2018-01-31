55aedae210dbae0123858ae8285fe7b25819fd3f
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 13:46:51 +0800

    feat: define snippet-detail component

--- source/components/snippet-detail.vue
+++ source/components/snippet-detail.vue
@@ -1,3 +1,85 @@
 <template>
-    <h1>Snippet Detail</h1>
+    <div class="snippet-detail">
+        <app-header></app-header>
+        <code-snippet ref="snippet" :value="snippet" readonly></code-snippet>
+        <app-footer :hash="hash"></app-footer>
+    </div>
 </template>
+
+<script>
+    import { mapState } from 'vuex';
+    import Shortcuts from '../mixins/shortcuts';
+    import store from '../store';
+    import { get } from '../api';
+
+    export default {
+        mixins: [Shortcuts],
+
+        props: {
+            hash: {
+                type: String,
+                required: true
+            }
+        },
+
+        computed: mapState(['snippet']),
+
+        methods: {
+            getShortcuts() {
+                return {
+                    KeyA: this.selectAll,
+                    KeyE: this.editNew,
+                    KeyD: this.duplicate,
+                    KeyR: this.raw
+                };
+            },
+
+            selectAll() {
+                this.$refs.snippet.select();
+            },
+
+            editNew() {
+                this.$store.commit('clearSnippet');
+                this.$router.push({ name: 'snippet-create' });
+            },
+
+            duplicate() {
+                this.$router.push({ name: 'snippet-create' });
+            },
+
+            raw() {
+                window.location.pathname = '/raw/' + this.hash;
+            }
+        },
+
+        components: {
+            'app-header': require('./app-header.vue').default,
+            'app-footer': require('./app-footer.vue').default,
+            'code-snippet': require('./code-snippet.vue').default
+        },
+
+        beforeRouteEnter(to, from, next) {
+            return get('/' + to.params.hash).then(response => {
+                store.commit('setSnippet', response.body);
+                next();
+            });
+        }
+    };
+</script>
+
+<style scoped>
+    .code-snippet {
+        position: fixed;
+        top: 16px;
+        left: 0;
+        right: 0;
+        bottom: 16px;
+    }
+
+    .app-footer {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
