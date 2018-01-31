49455cc03ec1b4403a948caa66ae2dac18bae18b
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 10:34:08 +0800

    feat: wip: initial vue ssr in home route

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,21 @@
-const { render } = require('server/reply');
+const fs = require('fs');
+const path = require('path');
+const { send } = require('server/reply');
+
+const templatePath = path.resolve(__dirname, '../views/index.html');
+const template = fs.readFileSync(templatePath, 'utf-8');
+
+const serverBundle = require('../public/vue-ssr-server-bundle.json');
+const clientManifest = require('../public/vue-ssr-client-manifest.json');
+const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
+    template,
+    clientManifest,
+    runInNewContext: false
+});
 
 async function home(ctx) {
-    return render('index.html');
+    const response = await renderer.renderToString({ url: ctx.url });
+    return send(response);
 }
 
 module.exports = home;
