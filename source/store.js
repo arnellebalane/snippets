d3d65564e7191f5f2f94ce631018a49b6bd7204a
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 11:15:23 +0800

    feat: define factory function for creating vuex stores

---
+++ source/store.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default function createStore() {
+    return new Vuex.Store({
+        state: {
+            snippet: null
+        },
+
+        mutations: {
+            setSnippet(state, snippet) {
+                state.snippet = snippet;
+            },
+
+            clearSnippet(state) {
+                state.snippet =  null;
+            }
+        }
+    });
+};
