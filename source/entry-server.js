67b4686368e16c939d7d8f29cf4302635e259a5b
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 14:42:22 +0800

    feat: prefetch component data inside server entrypoint script

--- source/entry-server.js
+++ source/entry-server.js
@@ -2,15 +2,24 @@ import createApp from './app';
 
 export default function(context) {
     return new Promise((resolve, reject) => {
-        const { app, router } = createApp();
+        const { app, router, store } = createApp();
 
         router.push(context.url);
 
         router.onReady(() => {
             const matchedComponents = router.getMatchedComponents();
-            return matchedComponents.length
-                ? resolve(app)
-                : reject({ code: 404 });
+            if (!matchedComponents.length) {
+                return reject({ code: 404 });
+            }
+
+            Promise.all(matchedComponents.map(Component => {
+                if (Component.serverData) {
+                    return Component.serverData(store, router.currentRoute);
+                }
+            })).then(() => {
+                context.state = store.state;
+                resolve(app);
+            });
         }, reject);
     });
 };
