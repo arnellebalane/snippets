b3fbe3ac16cd8def2fb5d1482805d72bdd334df9
Arnelle Balane <arnellebalane@gmail.com>
2018-01-31 14:04:34 +0800

    refactor: move ssr renderer into a module for reusability

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,5 @@
-const fs = require('fs');
-const path = require('path');
 const { send } = require('server/reply');
-
-const templatePath = path.resolve(__dirname, '../views/index.html');
-const template = fs.readFileSync(templatePath, 'utf-8');
-
-const serverBundle = require('../public/vue-ssr-server-bundle.json');
-const clientManifest = require('../public/vue-ssr-client-manifest.json');
-const renderer = require('vue-server-renderer').createBundleRenderer(serverBundle, {
-    template,
-    clientManifest,
-    runInNewContext: false
-});
+const renderer = require('./_renderer');
 
 async function home(ctx) {
     const response = await renderer.renderToString({ url: ctx.url });
