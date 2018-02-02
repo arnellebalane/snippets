96729baa587428dca34bc122ef86e996b6b553b5
Arnelle Balane <arnellebalane@gmail.com>
2018-02-03 00:04:45 +0800

    build(webpack): copy favicon image to build directory

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
+const CopyPlugin = require('copy-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
@@ -10,6 +11,9 @@ module.exports = merge(baseConfig, {
         client: path.resolve(__dirname, '../source/entry-client.js')
     },
     plugins: [
+        new CopyPlugin([ {
+            from: path.resolve(__dirname, '../source/images/favicon.png')
+        } ]),
         new UglifyJSPlugin(),
         new VueSSRClientPlugin(),
         new WorkboxPlugin({
