9503c98e0a2ee1df30d9f2fb29c6eed389032a78
Arnelle Balane <arnellebalane@gmail.com>
2018-02-23 15:48:05 +0800

    style(xo): additional auto-fixed linter errors

--- source/router.js
+++ source/router.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const routes = [ {
+const routes = [{
     path: '/',
     component: () => import(/* webpackChunkName: "snippet-create" */ './components/snippet-create.vue'),
     name: 'snippet-create'
@@ -12,11 +12,11 @@ const routes = [ {
     component: () => import(/* webpackChunkName: "snippet-detail" */ './components/snippet-detail.vue'),
     name: 'snippet-detail',
     props: true
-} ];
+}];
 
 export default function createRouter() {
     return new VueRouter({
         routes,
         mode: 'history'
     });
-};
+}
