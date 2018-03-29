ee2599f17d71cc6e5ace3c5a3b731257568b71ce
Arnelle Balane <arnellebalane@gmail.com>
2018-03-29 23:02:55 +0800

    refactor(ssr): remove vuejs server-side-rendering code

--- source/index.js
+++ source/index.js
@@ -3,10 +3,6 @@ import './stylesheets/index.css';
 
 const {app, router, store} = createApp();
 
-if (window.__INITIAL_STATE__) {
-    store.replaceState(window.__INITIAL_STATE__);
-}
-
 router.onReady(() => {
     router.beforeResolve((to, from, next) => {
         const matched = router.getMatchedComponents(to);
