1900270cf0504a67a980bd29a104e727309c2e37
Arnelle Balane <arnellebalane@gmail.com>
2018-03-30 22:35:53 +0800

    build(webpack): use html-webpack-plugin to generate html file in public dir

--- webpack/webpack.client.config.js
+++ webpack/webpack.client.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const HtmlPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
@@ -8,6 +9,9 @@ const baseConfig = require('./webpack.base.config');
 module.exports = merge(baseConfig, {
     entry: path.resolve(__dirname, '../source/static/index.js'),
     plugins: [
+        new HtmlPlugin({
+            template: path.resolve(__dirname, '../source/static/index.html')
+        }),
         new CopyPlugin([{
             from: path.resolve(__dirname, '../source/static/images/favicon.png')
         }]),
