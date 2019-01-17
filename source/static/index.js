2ba7e4979227f8f1bdf40ca1e62a3fdadb673eca
Arnelle Balane <arnellebalane@gmail.com>
2019-01-17 12:30:39 +0800

    Remove sw file and registration

--- source/static/index.js
+++ source/static/index.js
@@ -11,13 +11,3 @@ const app = new Vue({
     store: createStore(),
     render: h => h(SnippetsApp)
 });
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js').then(registration => {
-            console.log('Service worker registered', registration);
-        }).catch(e => {
-            console.warn('Service worker registration failed', e);
-        });
-    });
-}
--- source/static/sw.js
+++
@@ -1,6 +0,0 @@
-workbox.skipWaiting();
-workbox.clientsClaim();
-
-workbox.routing.registerRoute('/', workbox.strategies.cacheFirst());
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
