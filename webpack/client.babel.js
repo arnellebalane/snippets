cf3139f0fd00519cac1be9bafca6f1a0aeef0335
Arnelle Balane <arnellebalane@gmail.com>
2019-01-17 12:32:33 +0800

    Copy favicon using html plugin instead of using copy plugin

--- webpack/client.babel.js
+++ webpack/client.babel.js
@@ -1,6 +1,5 @@
 import merge from 'webpack-merge';
 import HtmlPlugin from 'html-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
 import baseConfig, {resolvePath} from './base.babel';
 
 module.exports = merge(baseConfig, {
@@ -8,11 +7,8 @@ module.exports = merge(baseConfig, {
 
     plugins: [
         new HtmlPlugin({
-            template: resolvePath('source/static/index.html')
-        }),
-
-        new CopyPlugin([{
-            from: resolvePath('source/static/images/favicon.png')
-        }])
+            template: resolvePath('source/static/index.html'),
+            favicon: resolvePath('source/static/images/favicon.png')
+        })
     ]
 });
