25129559fd061014a6a5e38adad23ef6a57f35f0
Arnelle Balane <arnellebalane@gmail.com>
2023-04-23 03:55:51 -0700

    Create ECR resources with CDK constructs

--- deploy/cdk/lib/cdk-stack.ts
+++ deploy/cdk/lib/cdk-stack.ts
@@ -1,7 +1,11 @@
+import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+import * as ecr_deployment from 'cdk-ecr-deployment';
 import { Construct } from 'constructs';
 
 export class CdkStack extends cdk.Stack {
@@ -31,7 +35,22 @@ export class CdkStack extends cdk.Stack {
         password: secret.secretValueFromJson('password'),
       },
       multiAz: false,
+      publiclyAccessible: true,
       vpc,
+    });
+
+    const backendRepository = new ecr.Repository(this, 'SnippetsBackendRepository', {
+      repositoryName: 'snippets-backend',
+      autoDeleteImages: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    const backendImage = new ecr_assets.DockerImageAsset(this, 'SnippetsBackendDockerImage', {
+      directory: path.resolve(__dirname, '../../../backend'),
+      file: 'deploy/Dockerfile',
+    });
+    new ecr_deployment.ECRDeployment(this, 'SnippetsBackendDeployment', {
+      src: new ecr_deployment.DockerImageName(backendImage.imageUri),
+      dest: new ecr_deployment.DockerImageName(`${backendRepository.repositoryUri}:latest`),
     });
   }
 }
