ef9545d483244a6f19955b18d5f7089e7c4254e3
Arnelle Balane <arnellebalane@gmail.com>
2023-06-21 00:53:06 -0700

    Add Lambda function for forwarding CF access logs to CloudWatch

---
+++ deploy/cdk/functions/cloudfront-logs-forwarder/index.ts
@@ -0,0 +1,105 @@
+import { promisify } from 'node:util';
+import { unzip as _unzip } from 'node:zlib';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { CloudWatchLogsClient, CreateLogStreamCommand, PutLogEventsCommand } from '@aws-sdk/client-cloudwatch-logs';
+import { Handler, S3Event } from 'aws-lambda';
+
+type CloudFrontAccessLog = Record<string, string> & { _timestamp: number };
+
+const s3 = new S3Client({});
+const logs = new CloudWatchLogsClient({});
+const unzip = promisify(_unzip);
+
+const logGroupName = 'TestCloudFrontLogsForwarderLogs';
+const logEventsPerBatch = 10_000;
+
+const createLogStream = async (logStreamName: string) => {
+    const request = new CreateLogStreamCommand({
+        logGroupName,
+        logStreamName,
+    });
+
+    try {
+        await logs.send(request);
+        console.info(`Log stream "${logStreamName}" in log group "${logGroupName}" was successfully created.`);
+    } catch (error: unknown) {
+        console.error(error);
+    }
+};
+
+const parseCloudFrontLogs = (content: string): CloudFrontAccessLog[] => {
+    const lines = content.trim().split(/\r?\n/g);
+    const fields = lines[1].replace('#Fields: ', '').split(/\s+/g);
+
+    return lines.slice(2).map((line) => {
+        const values = line.split(/\t/g);
+        const log = fields.reduce((result, field, i) => {
+            result[field] = values[i];
+            return result;
+        }, {} as CloudFrontAccessLog);
+        log._timestamp = new Date(`${log.date}T${log.time}+00:00`).valueOf();
+        return log;
+    });
+};
+
+const sendLogsToCloudWatch = async (logEvents: CloudFrontAccessLog[], logStreamName: string) => {
+    const batches = Math.ceil(logEvents.length / logEventsPerBatch);
+
+    for (let batch = 0; batch < batches; batch++) {
+        const start = batch * logEventsPerBatch;
+        const end = start + logEventsPerBatch;
+
+        const request = new PutLogEventsCommand({
+            logGroupName,
+            logStreamName,
+            logEvents: logEvents
+                .slice(start, end)
+                .map((logEvents) => ({
+                    timestamp: logEvents._timestamp,
+                    message: JSON.stringify(logEvents),
+                }))
+                .sort((a, b) => a.timestamp - b.timestamp),
+        });
+
+        try {
+            await logs.send(request);
+            console.info(
+                `Log events for log stream "${logStreamName}" in log group "${logGroupName}" were successfullly sent to CloudWatch.`
+            );
+        } catch (error: unknown) {
+            console.error(error);
+        }
+    }
+};
+
+export const handler: Handler = async (event: S3Event, context) => {
+    for (const record of event.Records) {
+        const Bucket = record.s3.bucket.name;
+        const Key = record.s3.object.key;
+
+        try {
+            const request = new GetObjectCommand({ Bucket, Key });
+            const response = await s3.send(request);
+            if (!response.Body) {
+                console.error(`Object "${Key}" from bucket "${Bucket}" is empty.`);
+                return;
+            }
+
+            let content;
+            if (response.ContentType === 'application/x-gzip' || Key.endsWith('.gz')) {
+                const buffer = await response.Body.transformToByteArray();
+                content = (await unzip(buffer)).toString('utf8');
+            } else {
+                content = await response.Body.transformToString();
+            }
+
+            const logs = parseCloudFrontLogs(content);
+            await createLogStream(Key);
+            await sendLogsToCloudWatch(logs, Key);
+        } catch (error) {
+            console.error(
+                `Error getting object "${Key}" from bucket "${Bucket}". Make sure they exist and the bucket is in the same region as this function.`
+            );
+        }
+    }
+};
