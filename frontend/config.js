ad8b82517ef741404c601e870612bf1bb36c9808
Arnelle Balane <arnellebalane@gmail.com>
2023-07-29 20:04:19 -0700

    Remove legacy implementation

--- frontend/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const { NODE_ENV = 'development', SNIPPETS_API_URL = 'http://localhost:3000' } = process.env;
-
-export default {
-  NODE_ENV,
-  API_URL: SNIPPETS_API_URL
-};
--- frontend/source/components/SnippetsApp.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div id="app" class="app">
-    <RouterView />
-  </div>
-</template>
