9eb9eb9a9135b0a4226a670d8a4460a7e4a6adf8
Arnelle Balane <arnellebalane@gmail.com>
2025-10-05 20:43:38 -0700

    Add proper error handling for Prisma queries

--- backend/express/index.ts
+++ backend/express/index.ts
@@ -1,4 +1,6 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import express from 'express';
+
 import { createSnippet, readSnippet } from '../core/snippets';
 
 const app = express();
@@ -7,13 +9,25 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.post('/api/snippets', async (req, res) => {
-    const snippet = await createSnippet(req.body.snippet);
-    res.json(snippet);
+    try {
+        const snippet = await createSnippet(req.body.snippet);
+        res.json(snippet);
+    } catch {
+        res.status(500);
+    }
 });
 
 app.get('/api/snippets/:hash', async (req, res) => {
-    const snippet = await readSnippet(req.params.hash);
-    res.send(snippet);
+    try {
+        const snippet = await readSnippet(req.params.hash);
+        res.send(snippet);
+    } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+            res.status(404);
+        } else {
+            res.status(500);
+        }
+    }
 });
 
 app.listen(port, () => {
