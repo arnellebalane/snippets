23ea1349c4540886123b0426d17bc426a410f986
Arnelle Balane <arnellebalane@gmail.com>
2025-10-05 20:56:36 -0700

    Update import path for Prisma client error classes

--- backend/express/index.ts
+++ backend/express/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import express from 'express';
 
 import { createSnippet, readSnippet } from '../core/snippets';
